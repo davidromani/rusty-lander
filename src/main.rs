e81ec52500de6d611c79c91bc0b5b3bab3d56f60
David Romaní <david@flux.cat>
2024-08-22 21:23:23 +0200

    add first System

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_systems(Update, print_hello_world_system)
+        .run()
+    ;
+}
+
+// systems
+fn print_hello_world_system() {
+    println!("Hello 'Rusty Lander' World!");
 }
