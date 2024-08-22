1e6f8db404f5016fcff812860d42e3519dcf8acb
David Romaní <david@flux.cat>
2024-08-22 21:26:17 +0200

    add first Components

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_systems(Startup, add_people_system)
         .add_systems(Update, print_hello_world_system)
         .run()
     ;
@@ -11,3 +12,16 @@ fn main() {
 fn print_hello_world_system() {
     println!("Hello 'Rusty Lander' World!");
 }
+
+fn add_people_system(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+// components
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
