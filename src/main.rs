13c55f22a3cd250be4827c6ac141e3faf6639ca3
David Romaní <david@flux.cat>
2024-08-22 21:36:37 +0200

    add plugin

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
         .add_systems(Startup, add_people_system)
-        .add_systems(Update, print_hello_world_system)
+        .add_systems(Update, (print_hello_world_system, greet_people_system))
         .run()
     ;
 }
@@ -19,9 +21,24 @@ fn add_people_system(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
+fn greet_people_system(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hi commander {}!", name.0);
+    }
+}
+
 // components
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+// plugins
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        // add things to your app here
+    }
+}
