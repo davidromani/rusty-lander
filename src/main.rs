93e8bd954698fc808914d40dd263c25be4d278e2
David Romaní <david@flux.cat>
2024-08-23 17:56:24 +0200

    execute greeting system every 3 secons

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,13 @@ fn add_people_system(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people_system(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hi commander {}!", name.0);
+fn greet_people_system(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hi commander {}!", name.0);
+        }
     }
 }
 
@@ -34,12 +38,17 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+// resources
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 // plugins
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people_system);
-        app.add_systems(Update, (print_hello_world_system, greet_people_system));
+        app.insert_resource(GreetTimer(Timer::from_seconds(3.0, TimerMode::Repeating)));
+        app.add_systems(Startup, (add_people_system, print_hello_world_system));
+        app.add_systems(Update, greet_people_system);
     }
 }
