977271be4c894161ef84b0c6d5aa39d046899fc7
David Romaní <david@flux.cat>
2024-08-24 16:10:28 +0200

    add handle_exit_key_pressed_system on run if condition

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::input::common_conditions::*;
+use bevy::app::AppExit;
 
 // const DEBUG_MODE: bool = true;
 // const IS_PLAYING: bool = true;
@@ -9,7 +11,7 @@ impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         //app.insert_resource(GameState { is_playing: IS_PLAYING });
         app.add_systems(Startup, setup_system); // runs only once at Startup sequence
-        // app.add_systems(Update, greet_people_system); // main App looper
+        app.add_systems(Update, handle_exit_key_pressed_system.run_if(input_just_pressed(KeyCode::Escape))); // main App looper
     }
 }
 
@@ -19,6 +21,11 @@ fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture: asset_server.load("background_space.png"),
         ..default()
     });
+}
+
+fn handle_exit_key_pressed_system(mut exit: EventWriter<AppExit>) {
+    info!("exit key has been pressed");
+    exit.send(AppExit::Success);
 }
 
 // Resources (global scope allocated data)
