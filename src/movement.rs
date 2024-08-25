0aa0b1f30af7901ead68db4b47556f702c0fc9de
David Romaní <david@flux.cat>
2024-08-25 19:36:48 +0200

    inver lander horizontal axis

--- src/movement.rs
+++ src/movement.rs
@@ -129,14 +129,14 @@ fn keyboard_input(
     let left = keyboard_input.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]);
     let right = keyboard_input.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]);
 
-    let horizontal = right as i8 - left as i8;
+    let horizontal = left as i8 - right as i8;
     let direction = horizontal as Scalar;
 
     if direction != 0.0 {
         movement_event_writer.send(MovementAction::Move(direction));
     }
 
-    if keyboard_input.just_pressed(KeyCode::Space) {
+    if keyboard_input.pressed(KeyCode::Space) {
         movement_event_writer.send(MovementAction::Jump);
     }
 
@@ -169,7 +169,7 @@ fn gamepad_input(
             button_type: GamepadButtonType::South,
         };
 
-        if buttons.just_pressed(jump_button) {
+        if buttons.pressed(jump_button) {
             movement_event_writer.send(MovementAction::Jump);
         }
     }
@@ -226,9 +226,11 @@ fn movement(
                     linear_velocity.x += *direction * movement_acceleration.0 * delta_time;
                 }
                 MovementAction::Jump => {
-                    if is_grounded {
-                        linear_velocity.y = jump_impulse.0;
-                    }
+                    // before
+                    // if is_grounded {
+                    //     linear_velocity.y = jump_impulse.0;
+                    // }
+                    linear_velocity.y += movement_acceleration.0 * delta_time;
                 }
             }
         }
