25c1a96f3aae0a3b39ffa0f69d67004ff7ee2475
David Romaní <david@flux.cat>
2024-08-25 16:21:11 +0200

    better main comments

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,22 @@ use spaceship::SpaceshipPlugin;
 
 fn main() {
     App::new()
-        // Bevy & Avian plugins
+        // Bevy & Avian2D plugins
         .add_plugins((
             DefaultPlugins,
-            // Add physics plugins and specify a units-per-meter scaling factor, 1 meter = 20 pixels.
-            // The unit allows the engine to tune its parameters for the scale of the world, improving stability.
             PhysicsPlugins::default().with_length_unit(20.0),
             PhysicsDebugPlugin::default(),
         ))
         // Resources
-        .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
+        .insert_resource(Gravity(Vector::NEG_Y * 9800.0))
         // Custom plugins
-        .add_plugins(AssetsLoaderPlugin)
-        .add_plugins(CameraPlugin)
-        .add_plugins(ColliderPlugin)
-        .add_plugins(DebugPlugin)
-        .add_plugins(GamePlugin)
-        .add_plugins(CharacterControllerPlugin)
-        .add_plugins(SpaceshipPlugin)
+        .add_plugins(AssetsLoaderPlugin) // startup
+        .add_plugins(CameraPlugin) // startup
+        .add_plugins(ColliderPlugin) // startup
+        .add_plugins(DebugPlugin) // startup
+        .add_plugins(GamePlugin) // post startup & update
+        .add_plugins(CharacterControllerPlugin) // update
+        .add_plugins(SpaceshipPlugin) // post startup
         .run()
     ;
 }
