d45bcb4ca7af34556122885873a6beb485fb2074
David Romaní <david@flux.cat>
2024-08-25 08:49:15 +0200

    add ColliderPlugin

---
+++ src/collider.rs
@@ -0,0 +1,19 @@
+use avian2d::prelude::*;
+use bevy::prelude::*;
+
+use crate::asset_loader::SceneAssets;
+
+pub struct ColliderPlugin;
+
+impl Plugin for ColliderPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_systems(PostStartup, intialize_land_system)
+        ;
+    }
+}
+
+// Systems
+fn intialize_land_system(mut commands: Commands, scene_assets: Res<SceneAssets>) {
+    commands.spawn((RigidBody::Static, Collider::circle(450.0)));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
 mod asset_loader;
-mod game;
 mod camera;
+mod collider;
+mod debug;
+mod game;
 mod movement;
 mod spaceship;
-mod debug;
 
+use avian2d::prelude::*;
 use bevy::prelude::*;
 
 use asset_loader::AssetsLoaderPlugin;
-use game::GamePlugin;
 use camera::CameraPlugin;
+use collider::ColliderPlugin;
+use debug::DebugPlugin;
+use game::GamePlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
-use debug::DebugPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        // Bevy & Avian plugins
+        .add_plugins((
+            DefaultPlugins, 
+            PhysicsPlugins::default(),
+            PhysicsDebugPlugin::default(),
+        ))
         // custom plugins
         .add_plugins(AssetsLoaderPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(ColliderPlugin)
         .add_plugins(DebugPlugin)
         .add_plugins(GamePlugin)
-        .add_plugins(CameraPlugin)
-        .add_plugins(SpaceshipPlugin)
         .add_plugins(MovementPlugin)
+        .add_plugins(SpaceshipPlugin)
         .run()
     ;
 }
