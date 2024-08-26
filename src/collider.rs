3d6769c940e1cbf90e3bd5ec79257db24b646934
David Romaní <david@flux.cat>
2024-08-26 23:51:02 +0200

    add 3 platforms

--- src/collider.rs
+++ src/collider.rs
@@ -25,19 +25,7 @@ fn intialize_landscape_system(
     scene_assets: Res<SceneAssets>,
     image_assets: Res<Assets<Image>>
 ) {
-    // bottom world bounds
-    commands.spawn((
-        Collider::rectangle(1200.0, 5.0),
-        RigidBody::Static,
-        MaterialMesh2dBundle {
-            mesh: meshes.add(Rectangle::new(1200.0, 5.0)).into(),
-            material: materials.add(Color::srgb(0.3, 0.3, 0.3)),
-            transform: Transform::from_xyz(0.0, -355.0, 1.0),
-            ..default()
-        },
-        DebugRender::default().with_collider_color(css::VIOLET.into()),
-    ));
-
+    // world bounds collider
     let vertices = vec![
         Vector::new(-630.0, 360.0),
         Vector::new(-630.0, -300.0),
@@ -50,7 +38,50 @@ fn intialize_landscape_system(
         polyline,
         DebugRender::default().with_collider_color(css::INDIAN_RED.into()),
     ));
-    
+
+    // platform x2
+    commands.spawn((
+        Collider::rectangle(100.0, 8.0),
+        RigidBody::Static,
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Rectangle::new(100.0, 8.0)).into(),
+            material: materials.add(Color::srgb(0.3, 0.3, 0.3)),
+            transform: Transform::from_xyz(100.0, 55.0, 1.0),
+            ..default()
+        },
+        Platform { factor: 2 },
+        DebugRender::default().with_collider_color(css::SPRING_GREEN.into()),
+    ));
+
+    // platform x5
+    commands.spawn((
+        Collider::rectangle(100.0, 8.0),
+        RigidBody::Static,
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Rectangle::new(100.0, 8.0)).into(),
+            material: materials.add(Color::srgb(0.3, 0.3, 0.3)),
+            transform: Transform::from_xyz(-260.0, -220.0, 1.0),
+            ..default()
+        },
+        Platform { factor: 5 },
+        DebugRender::default().with_collider_color(css::SPRING_GREEN.into()),
+    ));
+
+    // platform x10
+    commands.spawn((
+        Collider::rectangle(60.0, 8.0),
+        RigidBody::Static,
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Rectangle::new(60.0, 8.0)).into(),
+            material: materials.add(Color::srgb(0.3, 0.3, 0.3)),
+            transform: Transform::from_xyz(200.0, -95.0, 1.0),
+            ..default()
+        },
+        Platform { factor: 5 },
+        DebugRender::default().with_collider_color(css::SPRING_GREEN.into()),
+    ));
+
+    // land
     let sprite_image_handle = scene_assets.landscape.clone();
     info!("sprite_image_handle {:?}", sprite_image_handle);
     let sprite_image = image_assets.get(&sprite_image_handle);
@@ -63,11 +94,17 @@ fn intialize_landscape_system(
             texture: sprite_image_handle,
             transform: Transform {
                 translation: Vec3::new(0.0, -260.0, 1.0),
-                //scale: Vec3::new(2.0, 0.0, 1.0),
+                //scale: Vec3::fromnew(2.0, 0.0, 1.0),
                 ..default()
             },
             ..default()
         },
         DebugRender::default().with_collider_color(css::VIOLET.into()),
     ));
+}
+
+// Components
+#[derive(Component)]
+struct Platform {
+    factor: i8,
 }
