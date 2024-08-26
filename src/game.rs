57853e274e494c4358d6d9ff2f8dfea4f823aa72
David Romaní <david@flux.cat>
2024-08-26 08:02:01 +0200

    add a nice background rotation effect

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy::input::common_conditions::*;
 use bevy::app::AppExit;
+use std::f32::consts::TAU;
 
 use crate::asset_loader::SceneAssets;
 
@@ -15,21 +16,44 @@ impl Plugin for GamePlugin {
         app
             .add_systems(PostStartup, spawn_background_image_system) // runs only once at Startup sequence
             .add_systems(Update, handle_exit_key_pressed_system.run_if(input_just_pressed(KeyCode::Escape))) // main App looper
+            .add_systems(Update, rotate_background_image_system)
         ; 
     }
 }
 
 // Systems
 fn spawn_background_image_system(mut commands: Commands, scene_assets: Res<SceneAssets>) {
-    commands.spawn(SpriteBundle {
-        texture: scene_assets.background.clone(),
-        ..default()
-    });
+    commands.spawn((
+        SpriteBundle {
+            texture: scene_assets.background.clone(),
+            ..default()
+        },
+        Background,
+        Rotatable { speed: 0.001 }
+    ));
+}
+
+fn rotate_background_image_system(mut backgrounds: Query<(&mut Transform, &Rotatable), With<Background>>, timer: Res<Time>) {
+    for (mut transform, background) in &mut backgrounds { // TODO get only one queried object
+        // The speed is first multiplied by TAU which is a full rotation (360deg) in radians,
+        // and then multiplied by delta_seconds which is the time that passed last frame.
+        // In other words. Speed is equal to the amount of rotations per second.
+        transform.rotate_z(background.speed * TAU * timer.delta_seconds());
+    }
 }
 
 fn handle_exit_key_pressed_system(mut exit: EventWriter<AppExit>) {
     info!("exit key has been pressed");
     exit.send(AppExit::Success);
+}
+
+// Components
+#[derive(Component)]
+struct Background;
+
+#[derive(Component)]
+struct Rotatable {
+    speed: f32,
 }
 
 // Resources (global scope allocated data)
