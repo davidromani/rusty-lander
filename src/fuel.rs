ede60a6e6d16ff4504d1bf798c13faf203fb8048
David Romaní <david@flux.cat>
2024-09-01 08:42:07 +0200

    improve FuelPlugin again

--- src/fuel.rs
+++ src/fuel.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::input::common_conditions::*;
+use bevy::sprite::*;
 
 use crate::game::Scores;
 
@@ -8,7 +10,7 @@ impl Plugin for FuelPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Startup, spawn_fuel_bar_system)
-            .add_systems(Update, handle_fire_big_booster_key_pressed_system)
+            .add_systems(Update, handle_fire_big_booster_key_pressed_system.run_if(input_pressed(KeyCode::Digit2)))
         ;
     }
 }
@@ -17,29 +19,29 @@ impl Plugin for FuelPlugin {
 fn spawn_fuel_bar_system(mut commands: Commands) {
     commands.spawn((
         SpriteBundle {
-            transform: Transform::from_translation(Vec3::new(0.0, -320.0, 3.0)),
+            transform: Transform::from_translation(Vec3::new(-500.0, -340.0, 3.0)),
             sprite: Sprite {
-                color: Color::WHITE,
+                anchor: Anchor::CenterLeft,
+                color: Color::srgb(0.19, 0.10, 0.84),
                 custom_size: Some(Vec2::new(1000.0, 15.0)),
                 ..default()
             },
             ..default()
         },
-        FuelBar { fuel_quantity: 10000 },
+        FuelBar { fuel_quantity: 1000.0 },
     ));
 }
 
-fn handle_fire_big_booster_key_pressed_system(mut query: Query<(&mut FuelBar, &mut Sprite)>, scores: Res<Scores>, keyboard_input: Res<ButtonInput<KeyCode>>) {
-    if keyboard_input.pressed(KeyCode::Digit2) {
-        for (mut fuel_bar, sprite) in &mut query { // TODO get only one queried object
-            fuel_bar.fuel_quantity -= 1;
-            warn!("fuel_bar {:?} · sprite {:?} · scores {:?}", fuel_bar, sprite, scores);
-        }
+fn handle_fire_big_booster_key_pressed_system(mut query: Query<(&mut FuelBar, &mut Sprite)>, scores: Res<Scores>) {
+    for (mut fuel_bar, mut sprite) in &mut query { // TODO get only one queried object
+        fuel_bar.fuel_quantity -= 1.0;
+        sprite.custom_size = Some(Vec2::new(fuel_bar.fuel_quantity, 15.0));
+        warn!("fuel_bar {:?} · sprite {:?} · scores {:?}", fuel_bar, sprite, scores);
     }
 }
 
 // Components
 #[derive(Component, Debug)]
 struct FuelBar {
-    pub fuel_quantity: i32,
+    pub fuel_quantity: f32,
 }
