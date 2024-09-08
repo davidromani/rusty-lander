97e84e8b0dd64baa1edab0f3fd7eb829980d901c
David Romaní <david@flux.cat>
2024-09-08 12:04:24 +0200

    improve Fuel plugin GameState reactivity

--- src/fuel.rs
+++ src/fuel.rs
@@ -54,20 +54,32 @@ fn spawn_fuel_bar_system(mut commands: Commands) {
     ));
 }
 
-fn handle_fire_big_booster_key_pressed_system(mut scores: ResMut<Scores>, time: Res<Time>) {
-    if scores.fuel_quantity >= 0.0 {
+fn handle_fire_big_booster_key_pressed_system(
+    mut scores: ResMut<Scores>,
+    game_state: ResMut<State<GameState>>,
+    time: Res<Time>
+) {
+    if game_state.get() == &GameState::Landing && scores.fuel_quantity >= 0.0 {
         scores.fuel_quantity -= 100.0 * time.delta_seconds();
     }
 }
 
-fn handle_fire_medium_booster_key_pressed_system(mut scores: ResMut<Scores>, time: Res<Time>) {
-    if scores.fuel_quantity >= 0.0 {
+fn handle_fire_medium_booster_key_pressed_system(
+    mut scores: ResMut<Scores>,
+    game_state: ResMut<State<GameState>>,
+    time: Res<Time>
+) {
+    if game_state.get() == &GameState::Landing && scores.fuel_quantity >= 0.0 {
         scores.fuel_quantity -= 50.0 * time.delta_seconds();
     }
 }
 
-fn handle_fire_small_booster_key_pressed_system(mut scores: ResMut<Scores>, time: Res<Time>) {
-    if scores.fuel_quantity >= 0.0 {
+fn handle_fire_small_booster_key_pressed_system(
+    mut scores: ResMut<Scores>,
+    game_state: ResMut<State<GameState>>,
+    time: Res<Time>
+) {
+    if game_state.get() == &GameState::Landing && scores.fuel_quantity >= 0.0 {
         scores.fuel_quantity -= 20.0 * time.delta_seconds();
     }
 }
