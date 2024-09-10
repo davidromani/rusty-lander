fa2a952c078886d4a7cc7d64235acb17fd8ddfb9
David Romaní <david@flux.cat>
2024-09-10 21:09:23 +0200

    improve particles system - 6

--- src/particles_thruster.rs
+++ src/particles_thruster.rs
@@ -1,5 +1,5 @@
 use crate::game::Scores;
-use crate::spaceship::{Player, PlayerAction, VerticalThrusterEffect};
+use crate::spaceship::{HorizontalThrusterEffect, Player, PlayerAction, VerticalThrusterEffect};
 use crate::state::GameState;
 use bevy::prelude::*;
 use bevy_hanabi::prelude::*;
@@ -10,7 +10,13 @@ pub struct ParticlesThrusterPlugin;
 impl Plugin for ParticlesThrusterPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(HanabiPlugin)
-            .add_systems(Update, add_vertical_thrust_particles_to_spaceship_system)
+            .add_systems(
+                Update,
+                (
+                    add_vertical_thrust_particles_to_spaceship_system,
+                    add_left_thrust_particles_to_spaceship_system,
+                ),
+            )
             .add_systems(
                 Update,
                 update_vertical_thrust_particles_system.run_if(in_state(GameState::Landing)),
@@ -53,7 +59,7 @@ fn add_vertical_thrust_particles_to_spaceship_system(
             .init(SetAttributeModifier::new(Attribute::LIFETIME, lifetime))
             .render(ColorOverLifetimeModifier { gradient })
             .render(SizeOverLifetimeModifier {
-                gradient: Gradient::constant(Vec2::splat(2.)),
+                gradient: Gradient::constant(Vec2::splat(2.0)),
                 screen_space_size: true,
             }),
         );
@@ -74,6 +80,56 @@ fn add_vertical_thrust_particles_to_spaceship_system(
                     ..default()
                 },
                 VerticalThrusterEffect,
+            ));
+        });
+    }
+}
+
+fn add_left_thrust_particles_to_spaceship_system(
+    mut commands: Commands,
+    mut effects: ResMut<Assets<EffectAsset>>,
+    added_ships: Query<Entity, Added<Player>>,
+) {
+    for ship_entity in added_ships.iter() {
+        let writer = ExprWriter::new();
+        let lifetime = writer.lit(0.2).expr();
+        let mut gradient = Gradient::new();
+        gradient.add_key(0.0, Vec4::new(0.8, 0.8, 0.8, 0.9));
+        gradient.add_key(1.0, Vec4::ZERO);
+        let init_pos = SetPositionCone3dModifier {
+            height: writer.lit(-5.0).expr(),
+            base_radius: writer.lit(1.1).expr(),
+            top_radius: writer.lit(0.5).expr(),
+            dimension: ShapeDimension::Volume,
+        };
+        let init_vel = SetVelocitySphereModifier {
+            speed: writer.lit(100.0).uniform(writer.lit(400.0)).expr(),
+            center: writer.lit(Vec3::new(-1.0, 0.0, 0.0)).expr(),
+        };
+        let effect = effects.add(
+            EffectAsset::new(
+                vec![8012],
+                Spawner::once(10.0.into(), false),
+                writer.finish(),
+            )
+            .with_name("LeftThrusterEffect")
+            .init(init_pos)
+            .init(init_vel)
+            .init(SetAttributeModifier::new(Attribute::LIFETIME, lifetime))
+            .render(ColorOverLifetimeModifier { gradient })
+            .render(SizeOverLifetimeModifier {
+                gradient: Gradient::constant(Vec2::splat(2.0)),
+                screen_space_size: true,
+            }),
+        );
+        commands.entity(ship_entity).with_children(|parent| {
+            parent.spawn((
+                ParticleEffectBundle {
+                    effect: ParticleEffect::new(effect).with_z_layer_2d(Some(10.0)),
+                    transform: Transform::from_translation(Vec3::new(-28.0, 0.0, 0.0)),
+                    ..default()
+                },
+                HorizontalThrusterEffect,
             ));
         });
     }
