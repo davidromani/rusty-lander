e7e6eab49ab0623f2c6a60416903cc5dd3734b32
David Romaní <david@flux.cat>
2024-09-23 15:46:33 +0200

    enable icon copy build in scripts/build-mac-app.sh

--- scripts/build-mac-app.sh
+++ scripts/build-mac-app.sh
@@ -3,7 +3,7 @@ RUST_CRATE_NAME="rusty-lander"
 mkdir -p "${APP_NAME}.app/Contents/MacOS"
 mkdir -p "${APP_NAME}.app/Contents/Resources"
 cp Info.plist "${APP_NAME}.app/Contents/Info.plist"
-#cp AppIcon.icns "${APP_NAME}.app/Contents/Resources/AppIcon.icns"
+cp AppIcon.icns "${APP_NAME}.app/Contents/Resources/AppIcon.icns"
 cp -a assets "${APP_NAME}.app/Contents/MacOS/"
 cargo build --release --target x86_64-apple-darwin # build for Intel
 cargo build --release --target aarch64-apple-darwin # build for Apple Silicon
