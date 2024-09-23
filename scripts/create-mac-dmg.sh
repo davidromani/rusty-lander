b19b64771f55b6f12bf62b3bc5565aae1327a8da
David Romaní <david@flux.cat>
2024-09-23 16:10:40 +0200

    improve scripts/create-mac-dmg.sh

--- scripts/create-mac-dmg.sh
+++ scripts/create-mac-dmg.sh
@@ -2,4 +2,4 @@
 
 APP_NAME="Rusty Lander"
 mv "${APP_NAME}.app" build/mac/
-create-dmg --volname "${APP_NAME}" --volicon "AppIcon.icns" --window-size 800 400 --icon-size 128 --icon "${APP_NAME}.app" 200 200 --hide-extension "${APP_NAME}.app" --app-drop-link 600 200 "rusty_lander.dmg" "build/mac/"
+create-dmg --volname "${APP_NAME}" --window-size 800 400 --icon-size 128 --icon "${APP_NAME}.app" 200 200 --hide-extension "${APP_NAME}.app" --app-drop-link 600 200 "rusty_lander.dmg" "build/mac/"
