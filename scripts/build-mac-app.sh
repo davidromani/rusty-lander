bb3f8126e2385ffbadc81de837ebac0850903a86
David Romaní <david@flux.cat>
2024-09-23 13:25:58 +0200

    update scripts/build-mac-app.sh

--- scripts/build-mac-app.sh
+++ scripts/build-mac-app.sh
@@ -1,12 +1,12 @@
-APP_NAME="RustyLander"
-RUST_CRATE_NAME="rusty_lander"
+APP_NAME="Rusty Lander"
+RUST_CRATE_NAME="rusty-lander"
 mkdir -p "${APP_NAME}.app/Contents/MacOS"
 mkdir -p "${APP_NAME}.app/Contents/Resources"
-cp Info.plist "${APP_NAME}.app/Contents/Info.plist"
-cp AppIcon.icns "${APP_NAME}.app/Contents/Resources/AppIcon.icns"
-cp -a assets "${APP_NAME}.app/Contents/MacOS/"
-cargo build --release --target x86_64-apple-darwin # build for Intel
-cargo build --release --target aarch64-apple-darwin # build for Apple Silicon
-lipo "target/x86_64-apple-darwin/release/${RUST_CRATE_NAME}" \
-     "target/aarch64-apple-darwin/release/${RUST_CRATE_NAME}" \
-     -create -output "${APP_NAME}.app/Contents/MacOS/${APP_NAME}"
+#cp Info.plist "${APP_NAME}.app/Contents/Info.plist"
+#cp AppIcon.icns "${APP_NAME}.app/Contents/Resources/AppIcon.icns"
+#cp -a assets "${APP_NAME}.app/Contents/MacOS/"
+#cargo build --release --target x86_64-apple-darwin # build for Intel
+#cargo build --release --target aarch64-apple-darwin # build for Apple Silicon
+#lipo "target/x86_64-apple-darwin/release/${RUST_CRATE_NAME}" \
+#     "target/aarch64-apple-darwin/release/${RUST_CRATE_NAME}" \
+#     -create -output "${APP_NAME}.app/Contents/MacOS/${APP_NAME}"
