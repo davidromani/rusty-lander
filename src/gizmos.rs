45d4e799a778cdb070f1fecd26ba0970260436ef
David Romaní <david@flux.cat>
2024-11-02 19:37:12 +0100

    better gizmos coloring system

--- src/gizmos.rs
+++ src/gizmos.rs
@@ -2,7 +2,7 @@ use crate::collider::{PLATFORM_10X_CENTER, PLATFORM_2X_CENTER, PLATFORM_5X_CENTE
 use crate::game::InGameSet;
 use crate::spaceship::Player;
 use crate::state::GameState;
-use bevy::color::palettes::css::YELLOW_GREEN;
+use bevy::color::palettes::css::{LIGHT_SALMON, YELLOW_GREEN};
 use bevy::prelude::*;
 
 const PROXIMITY_RADIUS: f32 = 170.0;
@@ -23,24 +23,27 @@ impl Plugin for GizmosPlugin {
 // Systems
 fn draw_platform_gizmos_system(
     mut gizmos: Gizmos,
-    spaceship_query: Query<(&Transform), With<Player>>,
+    spaceship_query: Query<&Transform, With<Player>>,
 ) {
+    let mut color: Color;
     for transform in &spaceship_query {
-        if transform.translation.distance(PLATFORM_2X_CENTER) < PROXIMITY_RADIUS {}
-        gizmos.circle_2d(
-            PLATFORM_2X_CENTER.xy(),
-            PROXIMITY_RADIUS,
-            Color::from(YELLOW_GREEN),
-        );
-        gizmos.circle_2d(
-            PLATFORM_5X_CENTER.xy(),
-            PROXIMITY_RADIUS,
-            Color::from(YELLOW_GREEN),
-        );
-        gizmos.circle_2d(
-            PLATFORM_10X_CENTER.xy(),
-            PROXIMITY_RADIUS,
-            Color::from(YELLOW_GREEN),
-        );
+        if transform.translation.distance(PLATFORM_2X_CENTER) < PROXIMITY_RADIUS {
+            color = Color::from(LIGHT_SALMON);
+        } else {
+            color = Color::from(YELLOW_GREEN);
+        }
+        gizmos.circle_2d(PLATFORM_2X_CENTER.xy(), PROXIMITY_RADIUS, color);
+        if transform.translation.distance(PLATFORM_5X_CENTER) < PROXIMITY_RADIUS {
+            color = Color::from(LIGHT_SALMON);
+        } else {
+            color = Color::from(YELLOW_GREEN);
+        }
+        gizmos.circle_2d(PLATFORM_5X_CENTER.xy(), PROXIMITY_RADIUS, color);
+        if transform.translation.distance(PLATFORM_10X_CENTER) < PROXIMITY_RADIUS {
+            color = Color::from(LIGHT_SALMON);
+        } else {
+            color = Color::from(YELLOW_GREEN);
+        }
+        gizmos.circle_2d(PLATFORM_10X_CENTER.xy(), PROXIMITY_RADIUS, color);
     }
 }
